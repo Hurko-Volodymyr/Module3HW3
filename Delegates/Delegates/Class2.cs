42ed9cb568542ba59e179a6a768a4fcd63fff5c5
Hurko Volodymyr <92858780+Hurko-Volodymyr@users.noreply.github.com>
2022-10-15 22:11:01 +0300

    Done

---
+++ Delegates/Delegates/Class2.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegates
+{
+    internal class Class2
+    {
+        private readonly Class1 _class1 = new ();
+        private Predicate<int> _resultDelegate;
+        private int _calcResult;
+
+        public bool Status { get; set; }
+        public Func<int, int, int> PowDelegate { get; set; }
+
+        public Predicate<int> Calc(Func<int, int, int> powDelegate, int x, int y)
+        {
+            powDelegate = _class1.Pow;
+            _calcResult = powDelegate.Invoke(x, y);
+            _resultDelegate = Result;
+            return _resultDelegate;
+        }
+
+        public bool Result(int number)
+        {
+            Status = false;
+            PowDelegate = _class1.Pow;
+            if (_calcResult % number == 0)
+            {
+                Status = true;
+            }
+
+            return Status;
+        }
+    }
+}
--- Delegates/Delegates/Program.cs
+++ Delegates/Delegates/Program.cs
@@ -2,8 +2,24 @@
 {
     internal class Program
     {
+        public static void Show(bool status)
+        {
+            Console.WriteLine(status);
+        }
+
         private static void Main(string[] args)
         {
+            var class1 = new Class1();
+            var class2 = new Class2();
+            Console.WriteLine("True scenario:");
+            class2.Calc(class2.PowDelegate, 4, 4);
+            class2.Result(8);
+            class1.ShowHandler = Show;
+            class1.ShowHandler.Invoke(class2.Status);
+            Console.WriteLine("False scenario:");
+            class2.Calc(class2.PowDelegate, 3, 4);
+            class2.Result(5);
+            class1.ShowHandler.Invoke(class2.Status);
         }
     }
 }
